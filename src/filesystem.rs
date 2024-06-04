7368a918be83ab62511fafbe6052b852671561d4
Michael Robbins <michael@dalmura.com.au>
2024-06-04 23:14:49 +1000

    Produce hashed IDs and adding a leaf attribute to itemgroup

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha512};
 use std::fs;
 use std::path::{Path, PathBuf};
 
 /* generic functions */
-pub fn build_path(base_path: &String, category_path: &String) -> PathBuf {
-    Path::new(base_path).join(category_path)
+pub fn build_path(base_path: &String, next_item: &String) -> PathBuf {
+    Path::new(base_path).join(next_item)
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -13,42 +14,64 @@ pub(crate) struct ItemGroup {
     pub name: String,
     pub size_kb: u64,
     pub items: Vec<ItemGroup>,
+    pub leaf: bool,
 }
 
-fn dir_to_item(entry: fs::DirEntry) -> ItemGroup {
-    let children = match &build_items(entry.path(), false)[..] {
-        [] => build_items(entry.path(), true),
-        rest => rest.to_vec(),
-    };
+fn dir_to_item(entry: fs::DirEntry, parent_id: String) -> ItemGroup {
+    let filename = entry.file_name().into_string().unwrap();
+
+    let mut hasher = Sha512::new();
+    hasher.update(&parent_id);
+    hasher.update(&filename);
+
+    let item_id = format!("{:x}", hasher.finalize());
+
+    let mut children = build_items(entry.path(), item_id.clone(), false);
+    let mut leaf = false;
+
+    if children.is_empty() {
+        children = build_items(entry.path(), item_id.clone(), true);
+        leaf = true;
+    }
 
     ItemGroup {
-        id: entry.file_name().into_string().unwrap(),
-        name: entry.file_name().into_string().unwrap(),
+        id: item_id.clone(),
+        name: filename,
         size_kb: children.iter().map(|c| c.size_kb).sum(),
         items: children,
+        leaf,
     }
 }
 
-fn file_to_item(entry: fs::DirEntry) -> ItemGroup {
+fn file_to_item(entry: fs::DirEntry, parent_id: String) -> ItemGroup {
+    let filename = entry.file_name().into_string().unwrap();
+
+    let mut hasher = Sha512::new();
+    hasher.update(&parent_id);
+    hasher.update(&filename);
+
     ItemGroup {
-        id: entry.file_name().into_string().unwrap(),
-        name: entry.file_name().into_string().unwrap(),
+        id: format!("{:x}", hasher.finalize()),
+        name: filename,
         size_kb: entry.metadata().unwrap().len(),
         items: vec![],
+        leaf: false,
     }
 }
 
-pub fn build_items(category_path: PathBuf, leaf: bool) -> Vec<ItemGroup> {
-    match fs::read_dir(category_path) {
+pub fn build_items(item_path: PathBuf, parent_id: String, leaf: bool) -> Vec<ItemGroup> {
+    match fs::read_dir(item_path) {
         Err(why) => {
             println!("ERROR: Unable to list path: {:?}", why.kind());
             vec![]
         }
         Ok(paths) => match leaf {
-            true => paths.map(|i| file_to_item(i.unwrap())).collect(),
+            true => paths
+                .map(|i| file_to_item(i.unwrap(), parent_id.clone()))
+                .collect(),
             false => paths
                 .filter(|i| i.as_ref().unwrap().file_type().unwrap().is_dir())
-                .map(|i| dir_to_item(i.unwrap()))
+                .map(|i| dir_to_item(i.unwrap(), parent_id.clone()))
                 .collect(),
         },
     }
--- src/tasks.rs
+++ src/tasks.rs
@@ -7,6 +7,7 @@ use axum::{
     response::{Html, IntoResponse, Response},
     Json,
 };
+use sha2::{Digest, Sha512};
 
 pub async fn help() -> Html<&'static str> {
     Html("Please visit <a href='https://github.com/dalmura/stignore-agent'>the documentation</a> for further information")
@@ -18,14 +19,19 @@ pub async fn category_list(State(data): State<config::Data>) -> impl IntoRespons
         .categories
         .iter()
         .map(|c| {
+            let mut hasher = Sha512::new();
+            hasher.update(&c.id);
+            let parent_id = format!("{:x}", hasher.finalize());
+
             let category_path = filesystem::build_path(&data.agent.base_path, &c.relative_path);
-            let children = filesystem::build_items(category_path, false);
+            let children = filesystem::build_items(category_path, parent_id.clone(), false);
 
             filesystem::ItemGroup {
-                id: c.id.clone(),
+                id: parent_id,
                 name: c.name.clone(),
                 size_kb: children.iter().map(|c| c.size_kb).sum(),
                 items: children,
+                leaf: false,
             }
         })
         .collect();
@@ -40,6 +46,10 @@ pub async fn category_info(
 ) -> Response {
     match data.categories.iter().find(|x| x.id == category_id) {
         Some(category) => {
+            let mut hasher = Sha512::new();
+            hasher.update(&category.id);
+            let parent_id = format!("{:x}", hasher.finalize());
+
             let category_path =
                 filesystem::build_path(&data.agent.base_path, &category.relative_path);
 
@@ -47,7 +57,7 @@ pub async fn category_info(
                 StatusCode::OK,
                 Json(CategoryInfoResponse {
                     name: category.name.clone(),
-                    items: filesystem::build_items(category_path, false),
+                    items: filesystem::build_items(category_path, parent_id, false),
                 }),
             )
                 .into_response()
