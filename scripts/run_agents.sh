5eb9cc18aad8e9b7f5241cb6888e4e37133b0335
Michael Robbins <michael+github@dalmura.com.au>
2025-08-10 15:22:06 +1000

    Run agents waits and kills agents

--- scripts/run_agents.sh
+++ scripts/run_agents.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 cargo run config-agent1.toml &
+AGENT1=$!
 cargo run config-agent2.toml &
+AGENT2=$!
 cargo run config-agent3.toml &
+AGENT3=$!
 
-fg
+kill_agents() {
+    kill $AGENT1
+    kill $AGENT2
+    kill $AGENT3
+    exit 0
+}
+
+trap 'kill_agents' SIGINT
+
+print 'Agents running, CTRL+C to quit'
+while true; do
+    sleep 10
+done
