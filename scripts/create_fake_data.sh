67bdeffe5ec871bb32040a4f240df1080e769c54
Michael Robbins <michael+github@dalmura.com.au>
2025-08-22 23:22:07 +1000

    The fake data script now creates .st files in each category dir as well

--- scripts/create_fake_data.sh
+++ scripts/create_fake_data.sh
@@ -17,6 +17,9 @@ head -c 20M </dev/urandom >"${TARGET_DIR}/movies/Movie B (2024)/Movie B (2024).m
 mkdir -p "${TARGET_DIR}/movies/Movie C (2025)"
 head -c 30M </dev/urandom >"${TARGET_DIR}/movies/Movie C (2025)/Movie C (2025).mkv"
 
+touch "${TARGET_DIR}/movies/.stfolder"
+mkdir "${TARGET_DIR}/movies/.stversions"
+
 # TV Shows
 mkdir -p "${TARGET_DIR}/tv/Show A (1989)/Season 1"
 head -c 10M </dev/urandom >"${TARGET_DIR}/tv/Show A (1989)/Season 1/S01E01 - Ep 1 (1989).mkv"
@@ -43,3 +46,5 @@ head -c 10M </dev/urandom >"${TARGET_DIR}/tv/Show C (1999)/Season 1/S01E01 - Ep 
 head -c 10M </dev/urandom >"${TARGET_DIR}/tv/Show C (1999)/Season 1/S01E02 - Ep 2 (1999).mkv"
 head -c 10M </dev/urandom >"${TARGET_DIR}/tv/Show C (1999)/Season 1/S01E03 - Ep 3 (1999).mkv"
 
+touch "${TARGET_DIR}/tv/.stfolder"
+mkdir "${TARGET_DIR}/tv/.stversions"
